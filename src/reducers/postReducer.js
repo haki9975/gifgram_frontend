c4d98a97f09256e9ac51e8e7346d9ce2c46061c2
haki9975 <elias.robert.hakim@gmail.com>
2021-06-22 15:20:42 -0400

    Define initial state in postReducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,1 +1,10 @@
 import { FETCH_POSTS, NEW_POST, NEW_COMMENT } from '../actions/types';
+
+const initialState = {
+    items: [],
+    item: {}
+}
+
+export default function(state = initialState, action) {
+
+}
