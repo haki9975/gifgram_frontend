3de856dbbad1bd7a38124e9234c38ab518c14af7
haki9975 <elias.robert.hakim@gmail.com>
2021-06-22 15:07:26 -0400

    Import store from ./store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { Provider } from 'react-redux';
-
+import store from './store'
 
 import Posts from './components/Posts';
 import Postform from './components/Postform';
