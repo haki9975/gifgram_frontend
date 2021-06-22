ca417111226de6ff85437a898beb516053f43f2e
haki9975 <elias.robert.hakim@gmail.com>
2021-06-22 13:25:58 -0400

    Import each of the components to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 // import ReactDOM from 'react-dom';
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+import Comments from './components/Comments';
+import Commentform from './components/Commentform';
 
 function App() {
   return (
