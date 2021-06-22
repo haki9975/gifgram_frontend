958d0a87f3a28f60566bfd4eea14494d09a279aa
haki9975 <elias.robert.hakim@gmail.com>
2021-06-22 17:45:53 -0400

    Refactor fetch posts action

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,1 +1,11 @@
 import { FETCH_POSTS, NEW_POST, NEW_COMMENT } from './types';
+
+export const fetchPosts = () => {
+        fetch('https://localhost3000/posts')
+            .then(response => response.json())
+                .then (data => dispatch({
+                    type: FETCH_POSTS,
+                    payload: data
+                }))
+    
+}
