674927674faa852aa3a1b32dba48ad3dd36db9a1
haki9975 <elias.robert.hakim@gmail.com>
2021-06-23 13:10:25 -0400

    Refactor comment form to class component

--- src/components/Commentform.js
+++ src/components/Commentform.js
@@ -1,22 +1,37 @@
 import React from 'react'
 
-function Commentform() {
-    return (
-        <div>
-            <h1>Comment Form</h1>
-            <div>
-                <label>Username:</label><br />
-                <input type="text" name="username"></input><br />
-            </div>
-            <div>
-                <label>Comment:</label><br />
-                <textarea name="body"></textarea>
-            </div>
-            <br />
-            <button type="submit">Submit</button>
+class Commentform extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {value: ""};
+        this.handleChange.bind(this);
+        this.handleSubmit.bind(this);
+    }
 
-        </div>
-    )
+    handleChange(e) {
+        this.setState({value: e.target.value})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        alert("A comment was submitted:" + this.state.value);
+    }
+
+        render(){
+            return(
+                <div>
+                   <h1>Comment Form</h1>
+                   <form onSumit={this.handleSubmit}>
+                        <label>Username:</label><br />
+                        <input type="text" name="username"></input><br />
+                        <label>Comment:</label><br />
+                        <textarea name="body"></textarea>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            )
+        }
+    
 }
 
 export default Commentform;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,6 +3,12 @@ import { connect } from 'react-redux';
 import { fetchPosts, newPosts } from '../actions/postActions'
 
 function Posts() {
+    const postItems = this.state.posts.map(post => (
+        <div key={post.id}>
+            <h3>{post.username}</h3>
+            <img src={post.image}></img>
+        </div>
+    ))
     return (
         <div>
             <h1>Posts</h1>
