691696a79f935c8e877fd9037ef1927a47ad1273
haki9975 <elias.robert.hakim@gmail.com>
2021-06-27 11:22:58 -0400

    Import Router to App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import React from 'react';
  import { Provider } from 'react-redux';
  import store from './redux/store';
-
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 
 
