a8a7697648c2f623cb9ee7b323dde3c407304888
haki9975 <elias.robert.hakim@gmail.com>
2021-06-28 23:36:54 -0400

    Clean up Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,45 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPosts, sendLike, likePayload } from '../redux/postSlice';
-import Comments from './Comments';
-import Commentform from './Commentform';
 import SimpleCard from './Card';
-import Button from '@material-ui/core/Button';
-import  IconButton  from '@material-ui/core/IconButton';
 import PostCard from './PostsCard';
-
-
-
-
- 
-
 
 function Posts() {
 
     const posts = useSelector(state => state.posts)
     const dispatch = useDispatch()
 
-
-
     useEffect(() => {
         dispatch(fetchPosts())
         return () => {}
     }, [])
 
-
-
-    // const handleClick = (myLikes, id) => {
-    //     console.log("id", id)
-    //     // setLikes(myLikes +1)
-    //     // handleLike(likes, id)
-       
-    // }
-
-    // const handleLike = (likes, id) => {
-    //     const a = likePayload({id: id, likes: likes})
-    //     dispatch(sendLike(a))
-        
-    // }
     const renderPosts = posts.map((p) => {
       return(  <PostCard 
             id={p.id}
@@ -53,13 +27,8 @@ function Posts() {
     return (
         <div >
             <h1>Posts</h1>
-            <SimpleCard children={renderPosts.map((p) => p)}>
-                
-            </SimpleCard>
+            <SimpleCard children={renderPosts.map((p) => p)} />
         </div>
-           
-            
-        
     )
 }
 export default Posts;
